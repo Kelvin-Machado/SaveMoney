3a5a6af82777d315f1293895890410647f6a7b68
Kelvin Batista Machado <kelvinmachado@live.com>
2019-11-23 17:46:19 -0300

    Formats phone number

--- SaveMoney/Supporting Files/Others/TextFormatter.swift
+++ SaveMoney/Supporting Files/Others/TextFormatter.swift
@@ -84,3 +84,47 @@ extension CardController {
         }
     }
 }
+
+extension EmitenteController {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if (textField == telefoneTxt) {
+            let newString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+            let components = newString.components(separatedBy: NSCharacterSet.decimalDigits.inverted)
+
+            let decimalString = components.joined(separator: "") as NSString
+            let length = decimalString.length
+            let hasLeadingOne = length > 0 && decimalString.hasPrefix("1")
+
+            if length == 0 || (length > 11 && !hasLeadingOne) || length > 12 {
+                let newLength = (textField.text! as NSString).length + (string as NSString).length - range.length as Int
+
+                return (newLength > 11) ? false : true
+            }
+            var index = 0 as Int
+            let formattedString = NSMutableString()
+
+            if hasLeadingOne {
+                formattedString.append("1 ")
+                index += 1
+            }
+            if (length - index) > 2 {
+                let areaCode = decimalString.substring(with: NSMakeRange(index, 2))
+                formattedString.appendFormat("(%@)", areaCode)
+                index += 2
+            }
+            if length - index > 5 {
+                let prefix = decimalString.substring(with: NSMakeRange(index, 5))
+                formattedString.appendFormat("%@-", prefix)
+                index += 5
+            }
+
+            let remainder = decimalString.substring(from: index)
+            formattedString.append(remainder)
+            textField.text = formattedString as String
+            return false
+        }
+        else {
+            return true
+        }
+    }
+}
